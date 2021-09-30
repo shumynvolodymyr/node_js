73f91058150bd2a6bec2da931991954fd775f488
shumynvolodymyr <shumivova@gmail.com>
2021-09-30 09:20:15 +0300

    HW_1

---
+++ app.js
@@ -0,0 +1,41 @@
+const sorter = (directory) => {
+
+    const fs = require('fs');
+    const path = require('path');
+    const pathName = path.join(__dirname, `${directory}`)
+
+    fs.readdir(pathName, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        data.forEach(file => {
+
+            const pathFile = path.join(pathName, file);
+            const pathBoysFile = path.join(__dirname, 'boys', file);
+            const pathGirlsFile = path.join(__dirname, 'girls', file);
+
+            fs.readFile(pathFile, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return
+                }
+
+                const value = JSON.parse(data);
+
+                if (value.gender === "female") {
+                    fs.rename(pathFile, pathGirlsFile, err => {
+                        console.log(err);
+                    })
+                }
+                if (value.gender === "male") {
+                    fs.rename(pathFile, pathBoysFile, err => {
+                        console.log(err);
+                    });
+                }
+            });
+        });
+    });
+}
+
+sorter('boys');
