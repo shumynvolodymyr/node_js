ba91e9d156028ee598ef366aecf4411ffbbdcc9c
shumynvolodymyr <shumivova@gmail.com>
2021-10-12 18:06:05 +0300

    hw_5

--- routers/login.router.js
+++ routers/login.router.js
@@ -14,6 +14,7 @@ router.post(
 
 router.post(
     '/logout',
+    userAuthMiddleware.isLoginValid,
     userAuthMiddleware.isUserPresent,
     userAuthMiddleware.isPasswordMatched,
     loginController.logoutUser
